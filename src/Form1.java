3aa723685adbede0c1e577a00c2c99e0a21f2d7d
Alisson Viteri <alisson.viteri01@epn.edu.ec>
2024-06-21 12:26:52 -0500

    version1

---
+++ src/Form1.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Form1 {
+    public JPanel panel1;
+    private JLabel label_numero1;
+    private JTextField primer_numero;
+    private JLabel label_numero2;
+    private JTextField segundo_numero;
+    private JButton suma;
+    private JButton resta;
+    private JButton multiplicacion;
+    private JButton division;
+    private JLabel respuesta;
+
+    public Form1() {
+        suma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero1 = Double.parseDouble(primer_numero.getText());
+                double numero2 = Double.parseDouble(segundo_numero.getText());
+                double suma = numero1 + numero2;
+                respuesta.setText(String.valueOf(suma));
+            }
+        });
+        resta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero1 = Double.parseDouble(primer_numero.getText());
+                double numero2 = Double.parseDouble(segundo_numero.getText());
+                double resta = numero1 - numero2;
+                respuesta.setText(String.valueOf(resta));
+            }
+        });
+        multiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero1 = Double.parseDouble(primer_numero.getText());
+                double numero2 = Double.parseDouble(segundo_numero.getText());
+                double multiplicacion = numero1 * numero2;
+                respuesta.setText(String.valueOf(multiplicacion));
+            }
+        });
+    }
+}
